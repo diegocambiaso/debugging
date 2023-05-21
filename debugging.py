b1b1558c0ef09971a9125356231dae1ce3da0625
Diego Cambiaso <diegocambiaso@gmail.com>
2023-05-21 12:26:31 -0500

    Create debugging.py

---
+++ debugging.py
@@ -0,0 +1,10 @@
+import pdb; pdb.set_trace()
+
+while True:
+    try:
+        x = int(input("Please enter a number: "))
+        break
+    except ValueError:
+        print("Oops!  That was no valid number.  Try again...")
+
+print(x)
