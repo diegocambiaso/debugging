400a5499853d82254dc223580588b28356a91789
Diego Cambiaso <diegocambiaso@gmail.com>
2023-05-22 06:43:56 -0500

    Update debugging.py

--- debugging.py
+++ debugging.py
@@ -1,7 +1,9 @@
-import pdb; pdb.set_trace()
+# import pdb; pdb.set_trace() to use in Python till 3.7
+# breakpoint() is the new way stated in Python 3.8
 
 while True:
     try:
+        breakpoint()
         x = int(input("Please enter a number: "))
         break
     except ValueError:
